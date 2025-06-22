21eb055118ebcb277f30bd03c638501216b5100c
DJ O'Leary <dijitol@proton.me>
2025-06-22 16:25:41 +0200

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    outrig.url = "github:outrigdev/outrig";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.outrig.overlays.${system}.default
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputts = with pkgs; [
+            # Go
+            go
+            gopls
+            delve
+
+            # Build
+            docker-compose
+
+            # Tools
+            go-task
+            golangci-lint
+            outrig
+          ];
+        };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/djoleary/go_ddd_template
+
+go 1.24.4
+
+replace github.com/djoleary/go_ddd_template => ./
+
+tool github.com/outrigdev/outrig
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/outrigdev/outrig v0.8.2 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
+)
