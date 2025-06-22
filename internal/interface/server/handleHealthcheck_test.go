2c9297dba41f60dae2085c65f088c941b883649c
DJ O'Leary <dijitol@proton.me>
2025-06-23 01:28:46 +0200

    test: add test for /healthcheck

---
+++ internal/interface/server/handleHealthcheck_test.go
@@ -0,0 +1,24 @@
+package server_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/djoleary/go_ddd_template/internal/interface/server"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleHealthcheck(t *testing.T) {
+	mockEnv := testEnv{}
+	ws := echo.New()
+	s := server.NewServer(mockEnv, ws)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/healthcheck", nil)
+
+	s.ServeHTTP(w, r)
+
+	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+}
